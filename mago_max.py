d1162b33e8405a756bd17fa04a735e2a74780ab1
Daniregomez <34097882+Daniregomez@users.noreply.github.com>
2018-02-02 17:42:10 -0500

    Update mago_max.py

--- mago_max.py
+++ mago_max.py
@@ -13,6 +13,11 @@ def mago_max(lmt_inferior, lmt_superior, n, ng, fObjetivo):
     # Serie donde se guarda el mejor valor de fobj de cada generacion, para fines ilustrativos
     best_fobj = pd.Series()
 
+
+    # Lista donde se guardan todos los datos que seran regresados por la funcion
+    datos = list()
+
+
     for i in range(ng):
 
         print('Generacion numero: ', i + 1)
@@ -21,11 +26,13 @@ def mago_max(lmt_inferior, lmt_superior, n, ng, fObjetivo):
         resultados = evaluarPoblacion(poblacion, fObjetivo)
 
 
+        # Se agrega el mejor individuo de la generacion a best_fobj
+        best_fobj = best_fobj.append(pd.Series({i + 1 : resultados.index[-1]}))
+
+
         # Calculos estadisticos varios
         s = np.cov(poblacion, rowvar=False)
-
         sd = np.sqrt(np.diag(s))   
-
         media = np.mean(poblacion, axis=0)           
                 
         mini = np.subtract(media, np.multiply(0.5, sd) )
@@ -60,10 +67,18 @@ def mago_max(lmt_inferior, lmt_superior, n, ng, fObjetivo):
 
 
         # Poblacion de la siguiente generacion
-        poblacion = []
+        poblacion = list()
 
         if n1 != 0:
             poblacion.extend(grupo_mejores)
+        else:
+            # En caso de que n1 sea 0, guarda el mejor individuo para la proxima generacion
+            # y resta 1 a n3 o n1 para mantener la poblacion en n individuos
+            poblacion.append(resultados.iat[-1])
+            if n3 != 0:
+                n3 -= 1
+            else:
+                n2 -=1
 
         if n2 != 0:
             poblacion.extend(generarPoblacion(mini, maxi, n2))
@@ -72,13 +87,14 @@ def mago_max(lmt_inferior, lmt_superior, n, ng, fObjetivo):
             poblacion.extend(generarPoblacion(lmt_inferior, lmt_superior, n3))
 
 
-        best_fobj = best_fobj.append(pd.Series({i + 1 : fObjetivo(resultados.iat[-1])}))
+    datos.append("\nResultado:\n{} \n\nFuncion objetivo:\n{}\n ".format(resultados.iat[-1], resultados.index[-1]))
+    datos.append(best_fobj)
 
 
-    return ("\nResultado:\n{} \n\nFuncion objetivo:\n{}\n ".format(resultados.iat[0], fObjetivo(resultados.iat[0]))), best_fobj
+    return datos
 
 
-def generarPoblacion(limite_inferior,limite_superior, n):
+def generarPoblacion(limite_inferior, limite_superior, n):
     """
     Genera poblacion de manera uniforme, 
     primero una columna para cada variable de largo n, cada poblacion se 
@@ -105,7 +121,7 @@ def evaluarPoblacion(poblacion, fObjetivo):
     return resultados
 
 
-def filtrarIndividuosRegion(resultados,limite_inferior, limite_superior):
+def filtrarIndividuosRegion(resultados, limite_inferior, limite_superior):
     """
     Filtra los individuos segun limites inferiores y superiores y entrega una boolean mask
     """
@@ -124,6 +140,11 @@ def filtrarIndividuosRegion(resultados,limite_inferior, limite_superior):
 
 
 def competenciaG1(resultados, n1, sn, fobj, lim_inf, lim_sup):
+    """
+    Selecciona el grupo de los mejores individuos (cardinalidad n1) y los hace competir con una version
+    alterada de si mismos segun la formula 2 y 3 de la documentacion, en caso de que sea mejor se borra
+    el original y se agrega a la nueva version
+    """
 
     lng_pob = len(resultados)
     lng_lim = len(lim_inf)
@@ -132,10 +153,11 @@ def competenciaG1(resultados, n1, sn, fobj, lim_inf, lim_sup):
     mejorIndividuo = resultados.iat[-1]
    
     for ind in grupo_mejores:
-        xb = mejorIndividuo
+
+        xb = np.array(mejorIndividuo)
         b = np.random.randint(0, lng_pob)
-        xm = resultados.iat[b]
-        xt = ind + np.cross(sn, np.subtract(xb, xm)) 
+        xm = np.array(resultados.iat[b])
+        xt = ind + sn.dot(xb - xm)
 
         for i in range(lng_lim):
             if xt[i] < lim_inf[i]:
@@ -151,8 +173,3 @@ def competenciaG1(resultados, n1, sn, fobj, lim_inf, lim_sup):
     resultados.sort_index(inplace=True)
     
     return resultados
-
-
-
-
-
